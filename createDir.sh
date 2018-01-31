7357cfbe4ab3ebc98091599eb411e195b7331969
Maria Rabyk <mariia.rabyk@gmail.com>
2018-01-31 12:32:54 +0200

    Removed file that shouldn't be tracked

--- createDir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -p src/{main,test}/{java,resources,scala}
-mkdir lib project target
-
-# create an initial build.sbt file
-echo 'name := "URLShortener"
-version := "0.1"
-scalaVersion := "2.12.4"' > build.sbt
